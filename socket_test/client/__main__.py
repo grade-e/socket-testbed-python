95e5bbc171de0b2b1600db6a66b2b40ba6e2a8ed
Jinwoo Sung <jwsung91@gmail.com>
2025-03-27 22:53:39 +0900

    fix: add main.py to modules

--- socket_test/client/__main__.py
+++ socket_test/client/__main__.py
@@ -1,6 +1,9 @@
 from socket_test.client.client_handler import TCPClient
 from config import CLIENT_HOST, CLIENT_PORT, CLIENT_LOG_FILE
 
-if __name__ == "__main__":
+def main():
     client = TCPClient(CLIENT_HOST, CLIENT_PORT, CLIENT_LOG_FILE)
     client.run()
+
+if __name__ == "__main__":
+    main()
--- socket_test/server/__main__.py
+++ socket_test/server/__main__.py
@@ -1,6 +1,9 @@
 from socket_test.server.server_handler import TCPServer
 from config import SERVER_HOST, SERVER_PORT, SERVER_LOG_FILE
 
-if __name__ == "__main__":
+def main():
     server = TCPServer(SERVER_HOST, SERVER_PORT, SERVER_LOG_FILE)
     server.run()
+
+if __name__ == "__main__":
+    main()
